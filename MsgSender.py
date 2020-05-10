30d498ab4d40d67391cbce62e4edbe076af8359f
Ofir Ben Shoham <Ofir408@gmail.com>
2020-05-10 16:51:31 +0300

    Rename & comments were added

--- MsgSender.py
+++ MsgSender.py
@@ -2,8 +2,7 @@ import time
 from selenium import webdriver
 
 TIMEOUT = 5
-GOOGLE = 'https://www.google.com/'
-WHATSAPP = 'https://web.whatsapp.com/'
+WHATSAPP_URL = 'https://web.whatsapp.com/'
 
 # TODO - change it according yours.
 CHROME_DRIVER_PATH = r'C:/Users/Ofir/PycharmProjects/whatsapp_automation/drivers/chromedriver_version_81.exe'
@@ -13,10 +12,11 @@ class MsgSender:
 
     def send_whatsapp_message(self, message, contact):
         options = webdriver.ChromeOptions()
+        # save the session still doesn't work :(
         options.add_argument('--user-data-dir=./User_Data')  # Be careful with pushing the session to Github!!
         driver = webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=options)
 
-        driver.get(WHATSAPP)
+        driver.get(WHATSAPP_URL)
         time.sleep(TIMEOUT * 4)  # it takes time to scan the barcode.
         search_box = driver.find_element_by_xpath(f'//*[@title="{contact}"]')
         time.sleep(TIMEOUT)  # seconds
